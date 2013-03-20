c747123caca05743939b6e8dd3a20e930a152610
jlt <jlt@norriq.com>
2013-03-20 21:40:15 +0100

    lol

--- JSON.cs
+++ JSON.cs
@@ -12,8 +12,9 @@ namespace JONSParser
     {
         public static dynamic Parse(string str)
         {
-            var lexer = new Lexer(str);
-            var parser = new Parser(lexer);
+            var lexer = new Lxer(str);
+            var paser = new Parser(lexer);
+
             return parser.Parse();
         }
     }
